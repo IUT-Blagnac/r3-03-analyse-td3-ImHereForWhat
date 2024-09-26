855365723eb99d3bb6cabe2701583ff4d58d858c
laguilliez Mathys <laguilliezmathys1@gmail.com>
2024-09-26 12:18:08 +0200

    commit

--- trace/src/main/java/com/iutblagnac/r303trace/App.java
+++ trace/src/main/java/com/iutblagnac/r303trace/App.java
@@ -8,13 +8,22 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println(hello());
+        System.out.println(goodbye());
     }
 
     public static String hello() {
         return "Hello World!";
     }
 
+    public static String goodbye() {
+        return "See you World !";
+    }
+
     public static String hello(String param) {
+        return param;
+    }
+
+    public static String goodbye(String param) {
         return param;
     }
 
